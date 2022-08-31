ce0a47d2ec6466679b133fc39b28823841cdf401
psycode99 <georgefavour694@gmail.com>
2022-08-31 02:44:37 -0100

    second commit

--- space_invaders.py
+++ space_invaders.py
@@ -22,6 +22,7 @@ WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 YELLOW = (255, 255, 0)
 GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
 
 BULLET_VEL = 10
 MAX_BULLETS = 1
@@ -29,7 +30,13 @@ ALIEN_HIT = pygame.USEREVENT + 1
 EARTH_HIT = pygame.USEREVENT + 2
 WINNER_FONT = pygame.font.SysFont('Arial', 100)
 
+# hits
+LOW_HITS = 20
+MEDIUM_HITS = 30
+HIGH_HITS = 40
+
 SPACESHIP_WIDTH, SPACESHIP_HEIGHT = 65, 80
+EARTHSHIP_WIDTH, EARTHSHIP_HEIGHT = 40, 60
 BLOCKADE_WIDTH, BLOCKADE_HEIGHT = 170, 55
 
 EARTH_SPACESHIP_IMAGE = pygame.image.load(
@@ -74,121 +81,121 @@ SPACE_BACKGROUND = pygame.transform.scale(pygame.image.load(
 def window(earth_ship, purple_alien_ship, blue_ships,
            yellow_ships, danger_ships, earth_bullets, alien_bullets, alien_ships,
            big_ship, big_ship_bullets, blockades, hits):
+
+    WIN.blit(SPACE_BACKGROUND, (0, 0))
+    WIN.blit(EARTH_SPACESHIP, (earth_ship.x, earth_ship.y))
+
+    global PUR_SET
+    if purple_alien_ship.x >= -99 and PUR_SET:
+        purple_alien_ship.x += ALIEN_VEL
+    if purple_alien_ship.x > 700:
+        PUR_SET = False
+        purple_alien_ship.x -= 750
+    PUR_SET = True
+
+    # drawing alien ships
+    for ship in alien_ships:
+        if ship in alien_ships and ship in blue_ships:
+            WIN.blit(BLUE_SPACESHIP, (ship.x, ship.y))
+        elif ship in alien_ships and ship in yellow_ships:
+            WIN.blit(YELLOW_SPACESHIP, (ship.x, ship.y))
+        elif ship in alien_ships and ship in danger_ships:
+            WIN.blit(DANGER_SPACESHIP, (ship.x, ship.y))
+
+    # drawing big alien ship
+    for ship in big_ship:
+        if ship in big_ship:
+            WIN.blit(PURPLE_SPACESHIP, (purple_alien_ship.x, purple_alien_ship.y))
+
+    global SET
+    # CONTROL FOR TO AND FRO MOVEMENT OF ALIEN SHIPS... not perfect yet
+    if blue_ships[0].x >= 0 and SET:
+        blue_ships[0].x += ALIEN_VEL
+        blue_ships[1].x += ALIEN_VEL
+        blue_ships[2].x += ALIEN_VEL
+        blue_ships[3].x += ALIEN_VEL
+
+        yellow_ships[0].x += ALIEN_VEL
+        yellow_ships[1].x += ALIEN_VEL
+        yellow_ships[2].x += ALIEN_VEL
+        yellow_ships[3].x += ALIEN_VEL
+
+        danger_ships[0].x += ALIEN_VEL
+        danger_ships[1].x += ALIEN_VEL
+        danger_ships[2].x += ALIEN_VEL
+        danger_ships[3].x += ALIEN_VEL
+
+    if not SET or blue_ships[3].x > 650:
+        SET = False
+        if blue_ships[3].x > 300:
+            blue_ships[0].x -= ALIEN_VEL
+            blue_ships[1].x -= ALIEN_VEL
+            blue_ships[2].x -= ALIEN_VEL
+            blue_ships[3].x -= ALIEN_VEL
+
+            yellow_ships[0].x -= ALIEN_VEL
+            yellow_ships[1].x -= ALIEN_VEL
+            yellow_ships[2].x -= ALIEN_VEL
+            yellow_ships[3].x -= ALIEN_VEL
+
+            danger_ships[0].x -= ALIEN_VEL
+            danger_ships[1].x -= ALIEN_VEL
+            danger_ships[2].x -= ALIEN_VEL
+            danger_ships[3].x -= ALIEN_VEL
+
+            if blue_ships[3].x <= 300:
+                SET = True
+
+    # CONTROL FOR BARRICADES' COLOR CHANGES
     try:
-        WIN.blit(SPACE_BACKGROUND, (0, 0))
-        WIN.blit(EARTH_SPACESHIP, (earth_ship.x, earth_ship.y))
-
-        global PUR_SET
-        if purple_alien_ship.x >= -99 and PUR_SET:
-            purple_alien_ship.x += ALIEN_VEL
-        if purple_alien_ship.x > 700:
-            PUR_SET = False
-            purple_alien_ship.x -= 750
-        PUR_SET = True
-
-        # drawing alien ships
-        for ship in alien_ships:
-            if ship in alien_ships and ship in blue_ships:
-                WIN.blit(BLUE_SPACESHIP, (ship.x, ship.y))
-            elif ship in alien_ships and ship in yellow_ships:
-                WIN.blit(YELLOW_SPACESHIP, (ship.x, ship.y))
-            elif ship in alien_ships and ship in danger_ships:
-                WIN.blit(DANGER_SPACESHIP, (ship.x, ship.y))
-
-        # drawing big alien ship
-        for ship in big_ship:
-            if ship in big_ship:
-                WIN.blit(PURPLE_SPACESHIP, (purple_alien_ship.x, purple_alien_ship.y))
-
-        global SET
-        # CONTROL FOR TO AND FRO MOVEMENT OF ALIEN SHIPS... not perfect yet
-        if blue_ships[0].x >= 0 and SET:
-            blue_ships[0].x += ALIEN_VEL
-            blue_ships[1].x += ALIEN_VEL
-            blue_ships[2].x += ALIEN_VEL
-            blue_ships[3].x += ALIEN_VEL
-
-            yellow_ships[0].x += ALIEN_VEL
-            yellow_ships[1].x += ALIEN_VEL
-            yellow_ships[2].x += ALIEN_VEL
-            yellow_ships[3].x += ALIEN_VEL
-
-            danger_ships[0].x += ALIEN_VEL
-            danger_ships[1].x += ALIEN_VEL
-            danger_ships[2].x += ALIEN_VEL
-            danger_ships[3].x += ALIEN_VEL
-
-        if not SET or blue_ships[3].x > 650:
-            SET = False
-            if blue_ships[3].x > 300:
-                blue_ships[0].x -= ALIEN_VEL
-                blue_ships[1].x -= ALIEN_VEL
-                blue_ships[2].x -= ALIEN_VEL
-                blue_ships[3].x -= ALIEN_VEL
-
-                yellow_ships[0].x -= ALIEN_VEL
-                yellow_ships[1].x -= ALIEN_VEL
-                yellow_ships[2].x -= ALIEN_VEL
-                yellow_ships[3].x -= ALIEN_VEL
-
-                danger_ships[0].x -= ALIEN_VEL
-                danger_ships[1].x -= ALIEN_VEL
-                danger_ships[2].x -= ALIEN_VEL
-                danger_ships[3].x -= ALIEN_VEL
-
-                if blue_ships[3].x <= 300:
-                    SET = True
-
-        # CONTROL FOR BARRICADES' COLOR CHANGES
-        try:
-            if hits[0] > 7:
-                blockades.remove(blockades[0])
-            elif 5 < hits[0] <= 7:
-                pygame.draw.rect(WIN, RED, blockades[0])
-            elif 3 < hits[0] <= 5:
-                pygame.draw.rect(WIN, YELLOW, blockades[0])
-            elif hits[0] <= 3:
-                pygame.draw.rect(WIN, GREEN, blockades[0])
-        except IndexError:
-            pass
-
-        try:
-            if hits[1] > 7:
-                blockades.remove(blockades[1])
-            elif 5 < hits[1] <= 7:
-                pygame.draw.rect(WIN, RED, blockades[1])
-            elif 3 < hits[1] <= 5:
-                pygame.draw.rect(WIN, YELLOW, blockades[1])
-            elif hits[1] <= 3:
-                pygame.draw.rect(WIN, GREEN, blockades[1])
-        except IndexError:
-            pass
-
-        try:
-            if hits[2] > 7:
-                blockades.remove(blockades[2])
-            elif 5 < hits[2] <= 7:
-                pygame.draw.rect(WIN, RED, blockades[2])
-            elif 3 < hits[2] <= 5:
-                pygame.draw.rect(WIN, YELLOW, blockades[2])
-            elif hits[2] <= 3:
-                pygame.draw.rect(WIN, GREEN, blockades[2])
-        except IndexError:
-            pass
-
-        for bullet in earth_bullets:
-            pygame.draw.rect(WIN, RED, bullet)
-
-        for bullet in alien_bullets:
-            pygame.draw.rect(WIN, RED, bullet)
-
-        for bullet in big_ship_bullets:
-            pygame.draw.rect(WIN, RED, bullet)
-
-        pygame.display.update()
-
-    except pygame.error:
+        if hits[0] > HIGH_HITS:
+            blockades.remove(blockades[0])
+        if MEDIUM_HITS < hits[0] <= HIGH_HITS:
+            pygame.draw.rect(WIN, RED, blockades[0])
+        if LOW_HITS < hits[0] <= MEDIUM_HITS:
+            pygame.draw.rect(WIN, YELLOW, blockades[0])
+        if hits[0] <= LOW_HITS:
+            pygame.draw.rect(WIN, GREEN, blockades[0])
+    except IndexError:
         pass
+
+    try:
+        if hits[1] > HIGH_HITS:
+            blockades.remove(blockades[1])
+        if MEDIUM_HITS < hits[1] <= HIGH_HITS:
+            pygame.draw.rect(WIN, RED, blockades[1])
+        if LOW_HITS < hits[1] <= MEDIUM_HITS:
+            pygame.draw.rect(WIN, YELLOW, blockades[1])
+        if hits[1] <= LOW_HITS:
+            pygame.draw.rect(WIN, GREEN, blockades[1])
+    except IndexError:
+        pass
+
+    try:
+        if hits[2] > HIGH_HITS:
+            blockades.remove(blockades[2])
+        if MEDIUM_HITS < hits[2] <= HIGH_HITS:
+            pygame.draw.rect(WIN, RED, blockades[2])
+        if LOW_HITS < hits[2] <= MEDIUM_HITS:
+            pygame.draw.rect(WIN, YELLOW, blockades[2])
+        if hits[2] <= LOW_HITS:
+            pygame.draw.rect(WIN, GREEN, blockades[2])
+    except IndexError:
+        pass
+
+    for bullet in earth_bullets:
+        pygame.draw.rect(WIN, RED, bullet)
+
+    for bullet in alien_bullets:
+        pygame.draw.rect(WIN, BLUE, bullet)
+
+    for bullet in big_ship_bullets:
+        if len(big_ship) > 0:
+            pygame.draw.rect(WIN, YELLOW, bullet)
+        else:
+            big_ship_bullets.remove(bullet)
+
+    pygame.display.update()
 
 
 def draw_winner(text):
@@ -231,6 +238,14 @@ def bullets_handler(earth_bullets, earth_ship, alien_bullets,
     this is to be used to track the blockade color change
     :return:
     """
+    # checks if earth's bullets collide with a big ship
+    for ship in big_ship:
+        for bullet in earth_bullets:
+            if bullet.colliderect(ship):
+                pygame.event.post(pygame.event.Event(ALIEN_HIT))
+                big_ship.remove(ship)
+                earth_bullets.remove(bullet)
+                big_ship_bullets = []
     for bullet in earth_bullets:
         bullet.y -= BULLET_VEL
 
@@ -251,15 +266,6 @@ def bullets_handler(earth_bullets, earth_ship, alien_bullets,
             if bullet.colliderect(al_bullet):
                 earth_bullets.remove(bullet)
                 alien_bullets.remove(al_bullet)
-
-        # checks if earth's bullets collide with a big ship
-        for ship in big_ship:
-            if bullet.colliderect(ship):
-                pygame.event.post(pygame.event.Event(ALIEN_HIT))
-                big_ship.remove(ship)
-                earth_bullets.remove(bullet)
-                for big_bul in big_ship_bullets:
-                    big_ship_bullets.remove(big_bul)
 
         # checks if bullet exits the screen
         if bullet.y < 0:
@@ -282,6 +288,12 @@ def bullets_handler(earth_bullets, earth_ship, alien_bullets,
                 except IndexError:
                     pass
 
+        # checks if earth's bullets collide with an alien bullet
+        for er_bullet in earth_bullets:
+            if bullet.colliderect(er_bullet):
+                alien_bullets.remove(bullet)
+                earth_bullets.remove(er_bullet)
+
         # checks if alien bullet has hit earth_ship
         if bullet.colliderect(earth_ship):
             pygame.event.post(pygame.event.Event(EARTH_HIT))
@@ -297,6 +309,12 @@ def bullets_handler(earth_bullets, earth_ship, alien_bullets,
     for bullet in big_ship_bullets:
         if len(big_ship) > 0:
             bullet.y += BULLET_VEL
+
+        # checks if big ships' bullet has collided with earth's bullet
+        for er_bullet in earth_bullets:
+            if bullet.colliderect(er_bullet):
+                earth_bullets.remove(er_bullet)
+                big_ship_bullets.remove(bullet)
 
         # checks if big ship bullet has collided with a barricade
         for block in blockade:
@@ -326,7 +344,7 @@ def bullets_handler(earth_bullets, earth_ship, alien_bullets,
 
 def main():
     global select_ship
-    earth_spaceship = pygame.Rect(300, 600, SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
+    earth_spaceship = pygame.Rect(300, 600, EARTHSHIP_WIDTH, EARTHSHIP_HEIGHT)
 
     # list initiations. please forgive me
     earth_spaceship_bullets = []
@@ -377,21 +395,17 @@ def main():
             try:
                 select_ship = random.choice(all_alien_ships)
             except IndexError:
-                winner_text = 'You win'
-                draw_winner(winner_text)
-                main()
+                pass
 
             clock.tick(FPS)
 
-            with_x = 0
             choice = random.randint(1, 20)
             if choice == 12:
-                bullet = pygame.Rect(select_ship.x + 30, select_ship.y + 40, 5, 10)
+                bullet = pygame.Rect(select_ship.x + 40, select_ship.y + 30, 5, 10)
                 alien_ships_bullets.append(bullet)
-                with_x += 100
             elif choice == 13 and len(big_ship) > 0:
                 big_bullet = pygame.Rect(purple_alien_ship.x + 30,
-                                         purple_alien_ship.y + 40, 5, 10)
+                                         purple_alien_ship.y + 40, 10, 15)
                 big_ship_bullets.append(big_bullet)
 
             for event in pygame.event.get():
@@ -405,6 +419,14 @@ def main():
                                              earth_spaceship.y + 2, 5, 10)
                         earth_spaceship_bullets.append(bullet)
 
+            if len(all_alien_ships) == 0 and len(big_ship) == 0:
+                winner_text = 'You win'
+                draw_winner(winner_text)
+                global SET
+                global ALIEN_VEL
+                SET = True
+                ALIEN_VEL += 3
+                main()
             keys_pressed = pygame.key.get_pressed()
 
             # function calls
